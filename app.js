d3950f15b6cda6242cebc6b68b1d9c9b12ad20c4
Developer TariQul <99616512+developertariqulcom@users.noreply.github.com>
2022-03-19 20:57:23 +0600

    uplate
    update

--- app.js
+++ app.js
@@ -31,10 +31,9 @@ function generateRGBcolor() {
     const red = Math.floor (Math.random() *255);
     const green = Math.floor (Math.random() *255);
     const blue = Math.floor (Math.random() *255);
-    
-     return `rgb(${red}, ${green}, ${blue})`;
+     
 
-
+      return `rgb(${red}, ${green}, ${blue})`;
 }
 
 // step 3 -  collect  all  necessary  references
